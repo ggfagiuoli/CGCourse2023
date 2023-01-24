e5ff06f68581fe96372b6c1e9c481d81a2cd04de
ggfagiuoli <g.fagiuoli@studenti.unipi.it>
2023-01-24 20:41:08 +0100

    commit 1

---
+++ src/code_0_setup/HelloCGWorld/CMAKE/CMakeFiles/helloCGWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../helloCGWorld"
+  "../helloCGWorld.pdb"
+  "CMakeFiles/helloCGWorld.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloCGWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
