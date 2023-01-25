4542ee521ee9e1059b5ae12e54bfe64270cf7f9f
ggfagiuoli <g.fagiuoli@studenti.unipi.it>
2023-01-25 23:00:30 +0100

    Risolto problema con importazione libreria GLEW.

---
+++ src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/CMakeFiles/PROJECT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/PROJECT"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/PROJECT.pdb"
+  "CMakeFiles/PROJECT.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/context.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/egl_context.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/glx_context.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/init.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/input.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/linux_joystick.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/monitor.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/osmesa_context.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/posix_thread.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/posix_time.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/vulkan.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/window.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/x11_init.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/x11_monitor.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/x11_window.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src/xkb_unicode.c" "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/src/code_1_my_first_triangle/build-MyFirstTriangle-Desktop-Debug/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/include"
+  "/home/fagio/Documents/ComputerGraphics/fromGit/CGCourse2023/3dparty/glfw/src"
+  "glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
